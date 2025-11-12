43d71cd8b26a9167d7fce2dd13753e5350308d43
Pablo932002 <pablo5amadeo@gmail.com>
2025-11-12 22:10:29 +0100

    comentario en cerrar sesion

--- 004-Desarrollo de aplicaciones web utilizando código embebido/003-Seguridad usuarios, perfiles, roles/002-Ejercicios/RolesResuelto/cerrar_sesion.php
+++ 004-Desarrollo de aplicaciones web utilizando código embebido/003-Seguridad usuarios, perfiles, roles/002-Ejercicios/RolesResuelto/cerrar_sesion.php
@@ -2,6 +2,7 @@
 session_start();
 
 // Solo cerramos la sesión del usuario actual, sin eliminar los arrays globales
+// session_destroy(); si quisieramos cargarnos toda la sesión, pero como utilizamos variables de sesión para pasarnos el array de usuarios y el de roles no podemos hacerlo así.
 unset($_SESSION["usuario"]);
 unset($_SESSION["rol"]);
 
